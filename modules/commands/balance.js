d7aa582d358232a478964679b992ee53b0535904
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2018-10-03 13:21:22 +0200

    ok
    Co-Authored-By: Maika <themaikas@users.noreply.github.com>

--- modules/commands/balance.js
+++ modules/commands/balance.js
@@ -3,8 +3,9 @@ function run(message){
     const getBalance = require("./../getBalance.js");
     try {
         const userdata = require(`./../../saves/users/${message.author.id}.json`);
-        for(x in userdata.addresses){
-            message.channel.send(userdata.addresses[x].balance);
+        message.channel.send("Addresses from " + message.author.username); 
+        for(let x in userdata.addresses){
+            message.channel.send(x + ": " + userdata.addresses[x].balance);
         }
     } catch {
         message.channel.send("You haven't created an account yet!");
