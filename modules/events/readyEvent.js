81c710818b56606f6c8b0edb91f3cfaa13510a7b
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2018-10-07 20:01:20 +0200

    added new statuses for the bot

--- modules/events/readyEvent.js
+++ modules/events/readyEvent.js
@@ -1,22 +1,34 @@
 function run(client){
-  client.user.setActivity(`für ${client.users.array().length} Nutzer.`, { type:"PLAYING"});
-  setInterval(function() {
-    let statusint = Math.floor(Math.random() * Math.floor(4));
-    switch (statusint) {
-        case 0:
-            client.user.setActivity(`für ${client.users.array().length} Nutzer.`, { type: "PLAYING" });
-            break;
-        case 1:
-            client.user.setActivity(`auf ${client.guilds.array().length} Servern.`, { type: "WATCHING" });
-            break;
-        case 2:
-            client.user.setActivity(`in ${client.channels.array().length} Kanälen.`, { type: "LISTENING" });
-            break;
-        case 3:
-            client.user.setActivity("+createWallet", {type: "PLAYING"});
-            break;
-    }
-  }, 60000);
+    const diff = require("./../calculateDifficulty.js"); 
+    const blocks = require("./../../blockchain/blockchain.json").lastBlockNumber;
+    client.user.setActivity(`für ${client.users.array().length} Nutzer.`, { type:"PLAYING"});
+    console.log(client.guilds);
+    setInterval(function() {
+        let statusint = Math.floor(Math.random() * Math.floor(7));
+        switch (statusint) {
+            case 0:
+                client.user.setActivity(`für ${client.users.array().length} Nutzer.`, { type: "PLAYING" });
+                break;
+            case 1:
+                client.user.setActivity(`auf ${client.guilds.array().length} Servern.`, { type: "WATCHING" });
+                break;
+            case 2:
+                client.user.setActivity(`in ${client.channels.array().length} Kanälen.`, { type: "LISTENING" });
+                break;
+            case 3:
+                client.user.setActivity("+createWallet", {type: "PLAYING"});
+                break;
+            case 4:
+                client.user.setActivity("+help", {type: "PLAYING"});
+                break;
+            case 5:
+                client.user.setActivity(blocks + ' blocks', {type: "PLAYING"});
+                break;
+            case 6:
+                client.user.setActivity(diff.run(), {type: "PLAYING"});
+                break;
+        }
+  }, 30000);
 }
 
 module.exports = {
