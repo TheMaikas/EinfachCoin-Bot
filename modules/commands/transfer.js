f4c1677f8881cea3f0b4ec6b55c25ca026d9070a
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2018-10-15 20:52:19 +0200

    working on transfer module

--- modules/commands/transfer.js
+++ modules/commands/transfer.js
@@ -1,7 +1,8 @@
 const generateNewTxID = require("./../generateNewTxID.js");
 const fs = require('fs');
 
-var user = require(`./../../saves/users/${message.author.id}.json`);
+var pool; 
+var user; 
 var to;
 var amount;
 var fee;
@@ -9,24 +10,32 @@ var fee;
 
 function run(message){
 
+    pool = require("./../../saves/pool.json");
+    user = require(`./../../saves/users/${message.author.id}.json`);
+
     split = message.content.split(' ');
     to = split[1];
     amount = split[2];
     fee = split[3];
 
-    checkAndSave();
-    
+    checkAndSave(message);
 }
 
-function checkAndSave(){
+
+
+function checkAndSave(message){
     for(x in user.addresses){
-        if(x.balance > amount + fee){
+        console.log("hi");
+        if(user.addresses[x].balance > amount + fee){
+            console.log("hi 2")
+            
             var transactionid = generateNewTxID.run(message);
+            pool.fee[fee] = { transactionid: {from: x, to: to, amount: amount, fee: fee}};
             user.addresses[x].balance = user.addresses[x].balance - amount;
             user.addresses[x].transactions[transactionid] = {to: to, amount: amount};
             fs.writeFile(`./saves/users/${message.author.id}.json`, JSON.stringify(user, null, 4));
-            //in pool.json schreiben
-            //sender coins wegnehmen
+
+            fs.writeFile("./saves/pool.json", JSON.stringify(pool, null, 4));
             return;
         }
         
