4db87542e3d165ea6154ab8255161b5a96cf48e2
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2018-10-28 18:25:48 +0100

    updated balance

--- modules/commands/balance.js
+++ modules/commands/balance.js
@@ -5,20 +5,23 @@ function run(message) {
     if (message.mentions.users.array()[0] !== undefined) {
         try {
             var userdata = JSON.parse(fs.readFileSync(`./saves/users/${message.mentions.users.array()[0].id}.json`, 'utf-8'));
-            message.channel.send("Addresses from " + message.mentions.users.array()[0].id)
-                .then(msg => {
-                    msg.delete(60000);
-                });
+            var msg = "";
+            msg = msg + "Addresses from " + message.mentions.users.array()[0].id;
 
             for (let x in userdata.addresses) {
-                message.channel.send(x + ": " + userdata.addresses[x].balance);
+                msg = msg + "\n" + x + ": " + (parseInt(userdata.addresses[x].balance) / 100);
             }
+
+            message.channel.send(msg)
+                .then(balancemsg => {
+                    balancemsg.delete(60000);
+                });
 
         } catch (err) {
             message.channel.send("This user doesn't have a wallet yet!")
-            .then(msg => {
-                msg.delete(60000);
-            });
+                .then(msg => {
+                    msg.delete(60000);
+                });
         }
 
     } else {
@@ -26,24 +29,23 @@ function run(message) {
         try {
 
             var userdata = JSON.parse(fs.readFileSync(`./saves/users/${message.author.id}.json`, 'utf-8'));
-            message.channel.send("Addresses from " + message.author.username)
-            .then(msg => {
-                msg.delete(60000);
-            });
-            
+            var msg = "";
+            msg = msg + "Addresses from " + message.author.username;
 
             for (let x in userdata.addresses) {
-                message.channel.send(x + ": " + userdata.addresses[x].balance)
-                .then(msg => {
-                    msg.delete(60000);
-                });
+                msg = msg + "\n" + x + ": " + (parseInt(userdata.addresses[x].balance) / 100);
             }
+
+            message.channel.send(msg)
+            .then(balancemsg => {
+                balancemsg.delete(60000);
+            });
 
         } catch (err) {
             message.channel.send("You don't have a wallet yet!" + err)
-            .then(msg => {
-                msg.delete(60000);
-            });
+                .then(msg => {
+                    msg.delete(60000);
+                });
         }
 
     }
